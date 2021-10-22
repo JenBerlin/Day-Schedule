4fc9a78bd9782a8e758478e4da0a8e5e2f3e4892
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-10-22 18:07:30 +0200

    coloring interval script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 var today = moment();
 $("#currentDay").text(today.format('dddd, MMMM Do'))
+
+setInterval(() => {
+    const currentHour = moment().hour()
+    for (let hour = 0; hour < 24; hour++) {
+        const id = '#' + hour
+        if (hour < currentHour) {
+            $(id).addClass("past")
+        } else if (hour > currentHour) {
+            $(id).addClass("future")
+        } else {
+            $(id).addClass("present")
+        }
+    }
+}, 1000)
